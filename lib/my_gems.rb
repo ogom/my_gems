3331ba327d948540db89cf7eb100c763d40b2ed8
ogom <ogom@outlook.com>
2018-09-15 07:39:56 +0900

    Add FizzBuzz

--- lib/my_gems.rb
+++ lib/my_gems.rb
@@ -1,5 +1,16 @@
 require "my_gems/version"
 
 module MyGems
-  # Your code goes here...
+  class FizzBuzz
+    def initialize(number)
+      @number = number
+    end
+
+    def say
+      return 'FizzBuzz' if @number % 15 == 0
+      return 'Buzz' if @number % 5 == 0
+      return 'Fizz' if @number % 3 == 0
+      @number.to_s
+    end
+  end
 end
