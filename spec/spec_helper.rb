cec68924c0516f03a033de81c3324c94f75c5deb
ogom <ogom@outlook.com>
2018-09-22 11:41:58 +0900

    Fix spec helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,11 +1,9 @@
 # frozen_string_literal: true
 require 'bundler/setup'
 require 'simplecov'
-
 SimpleCov.start do
-  add_filter "/spec/"
+  add_filter '/spec/'
 end
-
 require 'my_gems'
 
 RSpec.configure do |config|
