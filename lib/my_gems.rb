dc0a6460b7421690b8fcd252701d24ee9e2b31eb
ogom <ogom@outlook.com>
2018-09-23 14:25:33 +0900

    Pass a test

--- lib/my_gems.rb
+++ lib/my_gems.rb
@@ -7,7 +7,11 @@ module MyGems
     end
 
     def say
-      ''
+      return 'FizzBuzz' if (@number % 15).zero?
+      return 'Buzz' if (@number % 5).zero?
+      return 'Fizz' if (@number % 3).zero?
+
+      @number.to_s
     end
   end
 end
