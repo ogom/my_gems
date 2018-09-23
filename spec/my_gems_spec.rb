82b829f999b7b7441aa1c75d7cdf2fbba296747b
ogom <ogom@outlook.com>
2018-09-23 14:22:41 +0900

    Test example

--- spec/my_gems_spec.rb
+++ spec/my_gems_spec.rb
@@ -2,20 +2,37 @@ require 'spec_helper'
 
 RSpec.describe MyGems do
   describe MyGems::FizzBuzz do
-    context '３で割り切れる場合' do
-      example 'Fizz という'
-    end
+    describe '#say' do
+      let(:fizz_buzz) { described_class.new(number) }
+      subject { fizz_buzz.say }
 
-    context '５で割り切れる場合' do
-      example 'Buzz という'
-    end
+      context '３で割り切れる場合' do
+        let(:number) { 3 }
+        example 'Fizz という' do
+          expect(subject).to eq 'Fizz'
+        end
+      end
 
-    context '１５で割り切れる場合' do
-      example 'FizzBuzz という'
-    end
+      context '５で割り切れる場合' do
+        let(:number) { 5 }
+        example 'Buzz という' do
+          expect(subject).to eq 'Buzz'
+        end
+      end
 
-    context '３や１５で割り切れない場合' do
-      example 'その 数字 をいう'
+      context '１５で割り切れる場合' do
+        let(:number) { 15 }
+        example 'FizzBuzz という' do
+          expect(subject).to eq 'FizzBuzz'
+        end
+      end
+
+      context '３や１５で割り切れない場合' do
+        let(:number) { 1 }
+        example 'その 数字 をいう' do
+          expect(subject).to eq '1'
+        end
+      end
     end
   end
 end
